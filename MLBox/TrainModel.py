bd085f0751a74d5662c6427df3cd5b61505a5a6a
Ryan Chynoweth <24chynoweth@gmail.com>
2019-05-06 16:06:38 -0700

    Adding initial mlbox code

--- MLBox/TrainModel.py
+++ MLBox/TrainModel.py
@@ -1,3 +1,19 @@
 from mlbox.preprocessing import *
 from mlbox.optimisation import *
 from mlbox.prediction import *
+
+
+train_path = ["./data/titanic_train.csv", "./data/titanic_test.csv"]
+
+
+reader = Reader(sep=",", header=0)
+data = reader.train_test_split(train_path, 'Survived')
+
+data = Drift_thresholder().fit_transform(data)
+
+Optimiser().evaluate(None, data)
+
+
+
+
+Predictor().fit_predict(None, data)
